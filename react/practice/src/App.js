e48c041055de00df26a6d57af7efec753f3f13ee
vividcolor12 <iny30221@naver.com>
2024-05-08 15:44:51 +0900

    LNY welcome

--- react/practice/src/App.js
+++ react/practice/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-   <div>
-     <p>素晴らしマン</p>
+    <div>
+      <p>素晴らしマン</p>
+      <p>안녕하세요 이남용 왔다감</p>
     </div>
   );
 }
